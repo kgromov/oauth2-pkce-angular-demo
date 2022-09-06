57e3cb01f304822a72d36aa63583fc9b7e5712a9
Konstantin <konstantin@revimedia.com>
2022-09-07 00:02:58 +0300

    Init commit

---
+++ backend/resource-server/src/test/java/oauth2/pkce/demo/ResourceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package oauth2.pkce.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
